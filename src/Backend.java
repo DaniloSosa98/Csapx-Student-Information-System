6fe1251858eecd533fa8fcf4c47ecbe78c168137
dgs5678 <dgs5678@g.rit.edu>
2019-10-16 13:46:26 -0400

    Defined 'Backend' methods

--- src/Backend.java
+++ src/Backend.java
@@ -1,2 +1,59 @@
+import java.util.Collection;
+
 public class Backend {
+    private CourseDB courseDB;
+    private UserDB userDB;
+
+    public Backend(String courseFile, String professorFile, String studentFile) {
+
+    }
+
+    private void addCourses(User user, String[] courseIds){
+
+    }
+
+    boolean courseExists(int id){
+
+    }
+
+    boolean enrollStudent(String username, int courseId){
+
+    }
+
+    Collection<Course> getAllCourses(){
+
+    }
+
+    Collection<User> getAllUsers(){
+
+    }
+
+    Course getCourse(int id){
+
+    }
+
+    Collection<Course> getCourseUser(String username){
+
+    }
+
+    private void initializeCourseDB(String courseFile){
+
+    }
+
+    private void initializeUserDB(String professorFile, String studentFile){
+
+    }
+
+    boolean isStudent(String username){
+
+    }
+
+    boolean unenrollStudent(String username, int courseId){
+
+    }
+
+    boolean userExists(String username){
+
+    }
+
 }
